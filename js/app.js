a376672d7ba4fbffb82bef58506834be2c597d97
NazariyM <nazarriko@gmail.com>
2016-12-23 15:06:52 +0200

    new nav

--- js/app.js
+++ js/app.js
@@ -12,4 +12,20 @@ $(document).ready(function () {
         });
     })();
 
+    // skype-call action
+    (function () {
+        var  skypeCall = $('.js-skype-call'),
+             FirstLinkName = skypeCall.find('ul li:first-child a'),
+             LastLinkName = skypeCall.find('ul li:last-child a'),
+             SkypeBody = skypeCall.children('div'),
+             SkypeImg = skypeCall.find('img'),
+             SkypeMessage = skypeCall.find('p a:not(ul li a)');
+
+        SkypeBody.css('display', 'inline-block');
+        SkypeImg.remove();
+        SkypeMessage.text('Связаться через Skype:').css('font-size', '13px');
+        FirstLinkName.text('Подзвонить');
+        LastLinkName.text('Написать');
+    })();
+
 });
